3b83ee4a42eea3489f205acae5753093902fe923
rolandkasa95 <rolandkasa95@gmail.com>
2016-06-23 12:27:52 +0300

    New: added the inArray validator

---
+++ Classes/Validator/inArray.php
@@ -0,0 +1,16 @@
+<?php
+
+class inArray
+{
+    public static $values;
+    public static function validate($value = null){
+        if(self::$values && in_array($value, self::$values)){
+            return true;
+        }
+            return false;
+    }
+
+    public static function setValues(array $values){
+        self::$values = $values;
+    }
+}
